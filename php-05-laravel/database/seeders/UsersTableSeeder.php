faa73f908f589dde1cb592dfe6f6f43a44e9ab63
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-08-10 16:10:45 +0300

    roles

---
+++ php-05-laravel/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for($i = 0; $i < 20; $i++ ){
+            $user = User::factory()->create();
+            $user->assignRole('user');
+        }
+
+
+    }
+}
