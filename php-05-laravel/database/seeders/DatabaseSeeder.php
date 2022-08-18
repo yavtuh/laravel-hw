b7562b0a7da3e1629fd5c543b63158fa844b0e19
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-08-18 22:14:22 +0300

    prodcatseed

--- php-05-laravel/database/seeders/DatabaseSeeder.php
+++ php-05-laravel/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersTableSeeder::class);
         $this->call(AdminUsersTableSeeder::class);
+        $this->call(CategoryProductsSeeder::class);
 
     }
 }
