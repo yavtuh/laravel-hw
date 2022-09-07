ba442c53bda1497f04d9778bd99eaba9cb9c4982
ugoldrovabriketu <103173469+ugoldrovabriketu@users.noreply.github.com>
2022-09-07 21:03:38 +0300

    rating

--- php-05-laravel/resources/views/products/show.blade.php
+++ php-05-laravel/resources/views/products/show.blade.php
@@ -18,7 +18,7 @@
             <p>Price: {{ $product->end_price }}$</p>
             <p>SKU: {{ $product->SKU }}</p>
             <p>In stock: {{ $product->in_stock }}</p>
-            {{--            <p>Rating: {{ round($product->averageRating(), 2) }}</p>--}}
+            <p>Rating: {{ round($product->averageRating(), 2) }}</p>
             <hr>
             <div>
                 <p>Product Category: <b> @include('categories.parts.category_view', ['category' => $product->category])</b></p>
@@ -45,54 +45,54 @@
                     </form>
                 </div>
             @endif
-            {{--            @auth--}}
-            {{--                <form class="form-horizontal poststars" action="{{ route('rating.add', $product) }}" id="addStar"--}}
-            {{--                      method="POST">--}}
-            {{--                    @csrf--}}
-            {{--                    <div class="form-group required">--}}
-            {{--                        <div class="col-sm-3 stars">--}}
-            {{--                            @if(!is_null($userRating))--}}
-            {{--                                @for($i = 5; $i >= 1; $i--)--}}
-            {{--                                    <input class="star star-{{$i}}"--}}
-            {{--                                           value="{{$i}}"--}}
-            {{--                                           id="star-{{$i}}"--}}
-            {{--                                           type="radio"--}}
-            {{--                                           name="star"--}}
-            {{--                                        {{--}}
-            {{--                                        $i == $userRating->rating--}}
-            {{--                                        ? 'checked'--}}
-            {{--                                        : ''--}}
-            {{--                                        }}--}}
-            {{--                                    />--}}
-            {{--                                    <label class="star star-{{$i}}" for="star-{{$i}}"></label>--}}
-            {{--                                @endfor--}}
-            {{--                            @else--}}
-            {{--                                <input class="star star-5" value="5" id="star-5" type="radio" name="star"/>--}}
-            {{--                                <label class="star star-5" for="star-5"></label>--}}
-            {{--                                <input class="star star-4" value="4" id="star-4" type="radio" name="star"/>--}}
-            {{--                                <label class="star star-4" for="star-4"></label>--}}
-            {{--                                <input class="star star-3" value="3" id="star-3" type="radio" name="star"/>--}}
-            {{--                                <label class="star star-3" for="star-3"></label>--}}
-            {{--                                <input class="star star-2" value="2" id="star-2" type="radio" name="star"/>--}}
-            {{--                                <label class="star star-2" for="star-2"></label>--}}
-            {{--                                <input class="star star-1" value="1" id="star-1" type="radio" name="star"/>--}}
-            {{--                                <label class="star star-1" for="star-1"></label>--}}
-            {{--                            @endif--}}
-            {{--                        </div>--}}
-            {{--                    </div>--}}
-            {{--                </form>--}}
-            {{--                <hr>--}}
-            {{--                @if(is_user_followed($product))--}}
-            {{--                    <form action="{{ route('wishlist.delete', $product) }}" method="POST">--}}
-            {{--                        @csrf--}}
-            {{--                        @method('DELETE')--}}
-            {{--                        <input type="submit" class="btn btn-danger" value="Remove from Wish List">--}}
-            {{--                    </form>--}}
-            {{--                @else--}}
-            {{--                    <a href="{{ route('wishlist.add', $product) }}"--}}
-            {{--                       class="btn btn-success">{{ __('Add to Wish List') }}</a>--}}
-            {{--                @endif--}}
-            {{--            @endauth--}}
+                        @auth
+                            <form class="form-horizontal poststars" action="{{ route('product.rating.add', $product) }}" id="addStar"
+                                  method="POST">
+                                @csrf
+                                <div class="form-group required">
+                                    <div class="col-sm-3 stars">
+                                        @if(!is_null($userRating))
+                                            @for($i = 5; $i >= 1; $i--)
+                                                <input class="star star-{{$i}}"
+                                                       value="{{$i}}"
+                                                       id="star-{{$i}}"
+                                                       type="radio"
+                                                       name="star"
+                                                    {{
+                                                    $i == $userRating->rating
+                                                    ? 'checked'
+                                                    : ''
+                                                    }}
+                                                />
+                                                <label class="star star-{{$i}}" for="star-{{$i}}"></label>
+                                            @endfor
+                                        @else
+                                            <input class="star star-5" value="5" id="star-5" type="radio" name="star"/>
+                                            <label class="star star-5" for="star-5"></label>
+                                            <input class="star star-4" value="4" id="star-4" type="radio" name="star"/>
+                                            <label class="star star-4" for="star-4"></label>
+                                            <input class="star star-3" value="3" id="star-3" type="radio" name="star"/>
+                                            <label class="star star-3" for="star-3"></label>
+                                            <input class="star star-2" value="2" id="star-2" type="radio" name="star"/>
+                                            <label class="star star-2" for="star-2"></label>
+                                            <input class="star star-1" value="1" id="star-1" type="radio" name="star"/>
+                                            <label class="star star-1" for="star-1"></label>
+                                        @endif
+                                    </div>
+                                </div>
+                            </form>
+                            <hr>
+{{--                            @if(is_user_followed($product))--}}
+{{--                                <form action="{{ route('wishlist.delete', $product) }}" method="POST">--}}
+{{--                                    @csrf--}}
+{{--                                    @method('DELETE')--}}
+{{--                                    <input type="submit" class="btn btn-danger" value="Remove from Wish List">--}}
+{{--                                </form>--}}
+{{--                            @else--}}
+{{--                                <a href="{{ route('wishlist.add', $product) }}"--}}
+{{--                                   class="btn btn-success">{{S __('Add to Wish List') }}</a>--}}
+{{--                            @endif--}}
+                        @endauth
         </div>
     </div>
     <hr>
@@ -123,6 +123,6 @@
     {{--        </div>--}}
     {{--    </div>--}}
 @endsection
-@push('scripts')
-    {{--    <script src="{{ h_secure_asset('js/product-actions.js') }}" type="text/javascript"></script>--}}
+@push('footer-scripts')
+    @vite(['resources/js/product-actions.js'])
 @endpush
